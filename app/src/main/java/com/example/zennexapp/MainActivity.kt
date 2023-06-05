2ff35264b2fedea4528db060f7520ac20f77e37b
Egorchik4 <EgorDanyluk72@mail.ru>
2023-06-05 15:32:22 +0700

    save instance of recycler view

--- app/src/main/java/com/example/zennexapp/MainActivity.kt
+++ app/src/main/java/com/example/zennexapp/MainActivity.kt
@@ -8,13 +8,27 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
+	companion object {
+
+		const val INSTANCE_KEY = "INSTANCE_KEY"
+		const val INSTANCE_VALUE = 1
+	}
+
 	override fun onCreate(savedInstanceState: Bundle?) {
 		setTheme(R.style.SplashScreen)
 		super.onCreate(savedInstanceState)
 		setContentView(R.layout.activity_main)
-		supportFragmentManager
-			.beginTransaction()
-			.replace(R.id.fragmentContainer, NewsListFragment())
-			.commit()
+		val instance = savedInstanceState?.getInt(INSTANCE_KEY)
+		if (instance == null) {
+			supportFragmentManager
+				.beginTransaction()
+				.replace(R.id.fragmentContainer, NewsListFragment())
+				.commit()
+		}
+	}
+
+	override fun onSaveInstanceState(outState: Bundle) {
+		super.onSaveInstanceState(outState)
+		outState.putInt(INSTANCE_KEY, INSTANCE_VALUE)
 	}
 }
